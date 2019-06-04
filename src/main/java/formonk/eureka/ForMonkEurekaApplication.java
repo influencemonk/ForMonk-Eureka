59dccbae5f5204d619c815aab4f352070ffbee84
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-06-05 02:58:29 +0530

    First commit

---
+++ src/main/java/formonk/eureka/ForMonkEurekaApplication.java
@@ -0,0 +1,15 @@
+package formonk.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ForMonkEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForMonkEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/formonk/eureka/ForMonkEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package formonk.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForMonkEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
